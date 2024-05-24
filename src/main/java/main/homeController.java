5a0bdcad837efadf90f0c9d32d4b9a58c7a7c06e
seunghyun <rs_next@naver.com>
2024-05-24 21:27:17 +0900

    update project

--- src/main/java/main/homeController.java
+++ src/main/java/main/homeController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class homeController {
 	
+	
+	
 	@RequestMapping("auditPageAdmin")
 	public String auditPageAdmin() {
 		return "auditPageAdmin";
 	
 	}
-	
+	 
 	@RequestMapping("auditMember")
 	public String auditMeber() {
 		return "admin/auditMember";
